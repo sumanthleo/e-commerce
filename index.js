1a6e69461279ac183820a01577be3bf4e72c3b44
sumanthleo <sumanthkonthala1998@gmail.com>
2022-06-02 21:24:14 +0530

    make it better

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 
 //routes
