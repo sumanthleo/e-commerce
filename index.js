6cf61a8c601963362cb8dc0a69f192ad5fde85d3
sumanthleo <sumanthkonthala1998@gmail.com>
2022-06-02 21:38:37 +0530

    make it better

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+
+app.use("/", router);
+
 app.use(
   cors({
     origin: "https://foodclone-app.netlify.app",
     methods: "GET,POST,PUT,DELETE",
   })
 );
-
-app.use(express.urlencoded({ extended: true }));
-
-//routes
-
-app.use("/", router);
 
 // dataBase
 connection();
