887970bda4267491a1d5e1c37311917bfc685c3f
sumanthleo <sumanthkonthala1998@gmail.com>
2022-06-02 21:41:42 +0530

    make it better

--- index.js
+++ index.js
@@ -9,18 +9,27 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// app.use(
+//   cors({
+//     origin: "https://foodclone-app.netlify.app",
+//     methods: "GET,POST,PUT,DELETE",
+//   })
+// );
+
 app.use(express.urlencoded({ extended: true }));
 
 //routes
 
 app.use("/", router);
 
-app.use(
-  cors({
-    origin: "https://foodclone-app.netlify.app",
-    methods: "GET,POST,PUT,DELETE",
-  })
-);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // dataBase
 connection();
