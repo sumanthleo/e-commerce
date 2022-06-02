de1767d62420c7e74118fca3573c29c923dda31e
sumanthleo <sumanthkonthala1998@gmail.com>
2022-06-02 14:55:57 +0530

    fixed untracked fileseses

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json({ extented: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
-app.use(cors(corsOptions));
 //routes
 
 app.use("/", router);
