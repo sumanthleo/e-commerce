6899c94940a07efa72b5c91761ce73e7126cd137
sumanthleo <sumanthkonthala1998@gmail.com>
2022-06-03 11:54:47 +0530

    make it bettersss

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //routes
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://foodclone-app.netlify.app"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(
   cors({
